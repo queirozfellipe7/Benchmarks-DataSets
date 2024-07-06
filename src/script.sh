5dcc9d7d3e8ae85d15f5981c3f70a173ecd97887
Fellipe Queiroz <queirozfellipe7@gmail.com>
2024-07-06 16:05:14 -0300

    Update script.sh

--- src/script.sh
+++ src/script.sh
@@ -20,22 +20,22 @@ done
 mv output_syr2k.csv   ../../../../../data
 cd -
 
-cd OpenMP/linear-algebra/kernels/syrk
+cd OpenMP/linear-algebra/kernels/atax
 pwd
-
 output=$(./jouleit.sh -l)
 output="$output;INPUT"
 output=${output//$'\n'/,}
-echo $output >> output_syrk.csv 
+echo $output >> output_atax.csv 
 
 for i in `seq 800 8 4000`; do
-	gcc -O2 -I../../../utilities syrk.c ../../../utilities/polybench.c -o syrk_acc -DNI=${i} -DNJ=${i} -fopenmp
-	output=$(./jouleit.sh -c ./syrk_acc -n)
+	gcc -O2 -I../../../utilities atax.c ../../../utilities/polybench.c -o atax_acc -DNX=${i} -DNY=${i} -fopenmp
+	
+	output=$(./jouleit.sh -c ./atax_acc -n)
 	output="$output;$i"
 	output=${output//$'\n'/,}
-	echo $output >> output_syrk.csv
+	echo $output >> output_atax.csv
 	echo $output
 done
-mv output_syrk.csv   ../../../../../data
+mv output_atax.csv   ../../../../../data
 cd -
 
